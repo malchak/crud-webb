1b5e93f12bdd0d19dd94890b7b5e683910000c47
malchak <michaelmalchak@gmail.com>
2013-10-25 15:58:28 -0700

    generate ember app architecture

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,13 @@
 //= require jquery
 //= require jquery_ujs
 //= require twitter/bootstrap
+//= require handlebars
+//= require ember
+//= require ember-data
+//= require_self
+//= require app
+
+// for more details see: http://emberjs.com/guides/application/
+App = Ember.Application.create();
+
 //= require_tree .
