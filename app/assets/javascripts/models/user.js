424acdd1d78e9adb94e2be9345d5e1a2f68cb0db
malchak <michaelmalchak@gmail.com>
2013-10-25 20:06:16 -0700

    create users list

--- app/assets/javascripts/models/user.js
+++ app/assets/javascripts/models/user.js
@@ -1,5 +1,9 @@
 App.User = DS.Model.extend({
 	firstName: DS.attr('string'),
 	lastName: DS.attr('string'),
-	quote: DS.attr('string')
+	quote: DS.attr('string'),
+	
+	fullName: (function(){
+		return this.get('firstName') + " " + this.get('lastName');
+	}).property('firstName', 'lastName')
 });
--- app/assets/javascripts/router.js
+++ app/assets/javascripts/router.js
@@ -1,5 +1,7 @@
 // For more information see: http://emberjs.com/guides/routing/
 
 App.Router.map(function() {
-  this.resource('users');
+  this.resource('users', function(){
+  	this.route('show', {path: '/:user_id'})
+  });
 });
